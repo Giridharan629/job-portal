5d849a25df15b274a0b5161c1036eca4a781d5e2
Giridharan C <118421351+Giridharan629@users.noreply.github.com>
2025-09-25 17:11:57 +0530

    initial commit

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -98,7 +98,7 @@ const JobCard = ({ job }) => {
             <line x1="12" y1="1" x2="12" y2="23"></line>
             <path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path>
           </svg>
-          <span>{job.type}</span>
+          <span>{job.salary}</span>
         </div>
       </div>
 
